89d254712643893d3e80ab543571979217f9faf5
Jerry Anselmi <janselmi@mppi.gob.ve>
2015-03-06 10:18:29 -0430

    Listo la personalización de los temas minimal y flash

--- src/WPace/WPace.php
+++ src/WPace/WPace.php
@@ -26,7 +26,12 @@ class WPace extends WPaceSettings
 
     private $url_theme_text = "%s%s%s.css";
 
-    private $theme_color_text = ".pace .pace-progress, .pace .pace-activity{background:%s !important;}";
+    //private $theme_color_text = ".pace .pace-progress, .pace .pace-activity{background:%s !important;}";
+
+    private $theme_color_text = array(
+        ".pace .pace-progress {background-color:{{color}};}",
+        ".pace .pace-progress {background-color:{{color}};}.pace .pace-progress-inner {box-shadow: 0 0 10px {{color}}, 0 0 5px {{color}};}.pace .pace-activity{ border-top-color:{{color}};border-left-color:{{color}};}"
+    );
 
     function __construct()
     {
@@ -38,12 +43,11 @@ class WPace extends WPaceSettings
 
     final public function include_scripts()
     {
-
         wp_enqueue_style('wpace-pace', $this->get_pace_theme());
         wp_add_inline_style('wpace-pace', "html body > *:not(.pace):not(script):not(style){display: none;}");
-        wp_add_inline_style('wpace-pace', sprintf($this->theme_color_text,$this->options["color-base"]));
-        wp_enqueue_script( 'wpace-pace','https://raw.githubusercontent.com/HubSpot/pace/v1.0.0/pace.min.js', '1.0.0', false );
-        wp_enqueue_script( 'wpace-core', WPACE_URL . 'inc/js/wpace.js', array('jquery','wpace-pace'), '1.0.0', false );
+        wp_add_inline_style('wpace-pace', $this->get_color_pace_theme());
+        wp_enqueue_script('wpace-pace','https://raw.githubusercontent.com/HubSpot/pace/v1.0.0/pace.min.js', '1.0.0', false );
+        wp_enqueue_script('wpace-core', WPACE_URL . 'inc/js/wpace.js', array('jquery','wpace-pace'), '1.0.0', false );
     }
 
     final public function include_scripts_admin()
@@ -62,7 +66,28 @@ class WPace extends WPaceSettings
 
     private function get_color_pace_theme()
     {
-        $this->options["color-base"];
+        $text = '';
+        $param = array("color" => $this->options["color-base"]);
+        switch ($this->options["tema-pace"])
+        {
+            case 1:
+                $text = $this->theme_color_text[1];
+                break;
+
+            default:
+                $text = $this->theme_color_text[0];
+                break;
+        }
+
+        return $this->mustache($text,$param);
+    }
+
+    private function mustache($text,$param)
+    {
+        foreach ($param as $key => $value) {
+            $text =  preg_replace("/{{{$key}}}/", $value, $text);
+        }
+        return $text;
     }
 }
 new WPace();
